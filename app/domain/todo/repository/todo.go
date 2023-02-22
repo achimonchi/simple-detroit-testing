74400f97aae4d529127380800baffb39e15cfe3d
Reyhan Jovie Dwiputra <reyhanjovie01@gmail.com>
2023-02-22 15:51:58 +0700

    add ref tags in drone

--- app/domain/todo/repository/todo.go
+++ app/domain/todo/repository/todo.go
@@ -1,9 +1,18 @@
 package repository
 
-import "database/sql"
+import (
+	"context"
+	"database/sql"
+	"detroit-testing/app/domain/todo/model"
+)
 
 type todoRepo struct {
 	db *sql.DB
+}
+
+// CreateTodo implements usecase.TodoRepo
+func (todoRepo) CreateTodo(ctx context.Context, todo model.Todo) error {
+	panic("unimplemented")
 }
 
 func NewTodoRepo(db *sql.DB) todoRepo {
